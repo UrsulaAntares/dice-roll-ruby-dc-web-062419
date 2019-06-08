b1bbb840a32194f5f62b9a356634c7720ee6e092
Ursula Miller <ursula.miller@gmail.com>
2019-06-08 23:29:47 +0000

    Done.

--- dice_roll.rb
+++ dice_roll.rb
@@ -2,6 +2,18 @@
 
 # Feel free to google "how to generate a random number in ruby"
 
+#def roll
+#  return rand(1..6)
+#end
+
+
 def roll
-  # code goes here
+  range_array = [1,2,3,4,5,6]
+  order = rand(0..6)
+  #puts order
+  puts range_array[order]
+  return range_array[order]
 end
+
+
+roll
